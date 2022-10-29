94f81fd97571f547cf4ccb16b9481e39b4651486
Rajeev Pai <rajeevpai.code@gmail.com>
2022-10-29 20:04:54 +0530

    Update mental health UI

--- mfa/src/app/mental-health/mental-health.component.ts
+++ mfa/src/app/mental-health/mental-health.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 
+type UserFitness = 'sad' | 'neutral';
+
+interface IUser {
+  id: string;
+  name: string;
+  image: string;
+  designation: string;
+  projects: string[];
+  inCharge: string;
+  reportedOn: number;
+  mentalFitness: UserFitness;
+}
+
 @Component({
   selector: 'mfa-mental-health',
   templateUrl: './mental-health.component.html',
-  styleUrls: ['./mental-health.component.scss']
+  styleUrls: ['./mental-health.component.scss'],
 })
 export class MentalHealthComponent implements OnInit {
+  readonly users: IUser[] = [
+    {
+      id: '1',
+      name: 'Susan',
+      image: 'assets/images/mental-fitness/users/user-1.svg',
+      designation: 'Frontend Engineer',
+      projects: ['Kesher'],
+      inCharge: 'Arun',
+      reportedOn: 123123,
+      mentalFitness: 'neutral',
+    },
+    {
+      id: '2',
+      name: 'Max',
+      image: 'assets/images/mental-fitness/users/user-2.svg',
+      designation: 'UI Designer',
+      projects: ['NMS', 'IBS'],
+      inCharge: 'Rahul',
+      reportedOn: 123123,
+      mentalFitness: 'sad',
+    },
+    {
+      id: '3',
+      name: 'Megan',
+      image: 'assets/images/mental-fitness/users/user-3.svg',
+      designation: 'Backend Engineer',
+      projects: ['Kesher'],
+      inCharge: 'Sreenath',
+      reportedOn: 123123,
+      mentalFitness: 'sad',
+    },
+    {
+      id: '4',
+      name: 'Tim',
+      image: 'assets/images/mental-fitness/users/user-4.svg',
+      designation: 'Intern',
+      projects: [],
+      inCharge: 'Fahad',
+      reportedOn: 123123,
+      mentalFitness: 'neutral',
+    },
+  ];
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getUserProjects(projects: string[]) {
+    if (projects.length === 0) {
+      return 'Not Assigned';
+    }
+
+    return projects.join(', ');
   }
 
+  getUserFitnessImage(fitness: UserFitness) {
+    return `assets/images/mental-fitness/status/${fitness}.svg`;
+  }
 }
