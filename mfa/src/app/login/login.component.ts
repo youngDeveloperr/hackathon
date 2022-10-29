7fbde07313cdbacb5b160bd539806eb21135ba40
Arun <arun@nintrivalabs.com>
2022-10-29 13:56:41 +0530

    Add login form functionality and routing

--- mfa/src/app/login/login.component.ts
+++ mfa/src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  ngOnInit(): void {
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigateByUrl('dashboard');
+    }
   }
 
+  ngOnInit(): void {}
 }
