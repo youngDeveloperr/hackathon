4c1252def9db87998b57676b1a907517b17278e1
Arun <arun@nintrivalabs.com>
2022-10-29 16:55:22 +0530

    Create mental health component

---
+++ mfa/src/app/mental-health/mental-health.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mfa-mental-health',
+  templateUrl: './mental-health.component.html',
+  styleUrls: ['./mental-health.component.scss']
+})
+export class MentalHealthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
