5b9932384064e85e06ced3cc78390f98e7ca9c57
Arun <arun@nintrivalabs.com>
2022-10-29 19:37:32 +0530

    Add user-dashboard tabs

--- mfa/src/app/shared/side-nav/side-nav.component.ts
+++ mfa/src/app/shared/side-nav/side-nav.component.ts
@@ -10,7 +10,7 @@ export class SideNavComponent implements OnInit {
   constructor(private router: Router) {}
 
   activeTab: string = '';
-  sideNav = [
+  adminSideNav = [
     {
       title: 'DashBoard',
       image: 'assets/images/dashboard.svg',
@@ -57,6 +57,43 @@ export class SideNavComponent implements OnInit {
       path: '/health',
     },
   ];
+  userSideNav=[
+  {
+    title: 'Team',
+    image: 'assets/images/teams.svg',
+    path: '/list',
+  },
+  {
+    title: 'Vacations',
+    image: 'assets/images/vacation.svg',
+    path: '/vacation',
+  },
+  {
+    title: 'Time Tracking',
+    image: 'assets/images/time.svg',
+    path: '/time',
+  },
+  {
+    title: 'Reports',
+    image: 'assets/images/reports.svg',
+    path: '/reports',
+  },
+  {
+    title: 'Calendar',
+    image: 'assets/images/calendar.svg',
+    path: '/calendar',
+  },
+  {
+    title: 'Notification',
+    image: 'assets/images/dash-mail.svg',
+    path: '/notification',
+  },
+  {
+    title: 'Settings',
+    image: 'assets/images/settings.svg',
+    path: '/settings',
+  },
+  ]
 
   selectedNav(id: string) {
     this.activeTab = id;
